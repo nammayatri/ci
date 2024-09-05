7a7f3e4308016ed6e4d949b2cf2411ccbf8d2956
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-09-05 19:54:21 -0400

    config: Add comment explaining 'admins'

--- common/config/default.nix
+++ common/config/default.nix
@@ -1,5 +1,6 @@
 {
   adminUser = "admin";
+  # Users who are authorized to SSH as `adminUser` (see above) to our hosts
   admins = {
     srid = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd";
   };
