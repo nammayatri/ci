dc7766b87a850a58ebed59338f32c4daed22d498
Sridhar Ratnakumar <srid@srid.ca>
2024-10-21 18:24:36 -0400

    refactor: Use nixos-unified autowiring of toplevel

--- flake.nix
+++ flake.nix
@@ -1,51 +1,14 @@
 {
   inputs = {
-    # Principle inputs (updated by `nix run .#update`)
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
     ragenix.url = "github:yaxitech/ragenix";
+    nixos-unified.url = "github:srid/nixos-unified";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, ... }:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
-
-      flake.om.ci.default =
-        let
-          hostsConfig =
-            let
-              inherit (inputs.nixpkgs.lib) listToAttrs nameValuePair attrNames;
-              inherit (builtins) map readDir;
-              overrideInputs = {
-                common = ./common;
-              };
-              hosts = attrNames (readDir ./hosts);
-              systems = {
-                ny-ci-nixos = [ "x86_64-linux" ];
-              };
-              configForHost = name: {
-                inherit overrideInputs;
-                systems = systems.${name};
-                dir = "./hosts/${name}";
-              };
-            in
-            listToAttrs (map (name: nameValuePair name (configForHost name)) hosts);
-        in
-        hostsConfig // {
-          root.dir = ".";
-        };
-
-      perSystem = { inputs', pkgs, ... }: {
-        formatter = pkgs.nixpkgs-fmt;
-
-        devShells.default = pkgs.mkShell {
-          name = "ny-ci-shell";
-          packages = with pkgs; [
-            just
-            nixd
-            inputs'.ragenix.packages.default
-          ];
-        };
-      };
-    };
+  outputs = inputs:
+    inputs.nixos-unified.lib.mkFlake { inherit inputs; root = ./.; };
 }
