2f6c104634b7d12982caeceb343bdf62006e15e5
Sridhar Ratnakumar <srid@srid.ca>
2024-11-13 09:54:28 -0500

    Workaround nm issue

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -6,4 +6,7 @@
     ./base.nix
     ./home.nix
   ];
+
+  # https://discourse.nixos.org/t/nixos-rebuild-switch-upgrade-networkmanager-wait-online-service-failure/30746/2
+  systemd.services.NetworkManager-wait-online.enable = false;
 }
