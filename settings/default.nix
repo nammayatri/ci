216f882e4c72e9e2fed27f747300337ae5bbe078
Sridhar Ratnakumar <srid@srid.ca>
2025-03-28 09:29:46 -0400

    Add sai

--- settings/default.nix
+++ settings/default.nix
@@ -3,5 +3,6 @@
   # Users who are authorized to SSH as `adminUser` (see above) to our hosts
   admins = {
     srid = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd";
+    sai = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO+CxWHmAzQqAThKKdUE96IjZa/LG64XMtyZZ/y1o1GY";
   };
 }
